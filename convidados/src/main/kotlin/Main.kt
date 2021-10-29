fa18e25423b3ecf88b12a939c58ead7be56ce87b
cristhian-ruescas <cristhian.ruescas83@gmail.com>
2021-10-29 14:18:31 -0300

    Comentario e outros...

--- convidados/src/main/kotlin/Main.kt
+++ convidados/src/main/kotlin/Main.kt
@@ -1,14 +1,14 @@
 fun main(args: Array<String>) {
 
-    var sair = "Sim"
-    val convidados = mutableMapOf(
+    var sair = "Sim" // variavel para parar o while
+    val convidados = mutableMapOf(     // A lista
         1 to "Maria",
         2 to "Fabiano",
         3 to "Cauã",
         4 to "Mariana"
     )
 
-    while (sair == "Sim") {
+    while (sair == "Sim") {  // laço de repetição para que possa adicionar ou remover varias vezes
 
         println(
             "Escolha dentre as opções:  1° adiciona um novo convidade - 2° remover um convidade - 3°listar convidades" +
@@ -16,29 +16,29 @@ fun main(args: Array<String>) {
         )
         var opcao = readLine()!!.toInt()
 
-        if (opcao == 1) {
+        if (opcao == 1) {                                  // para adicionar,digitando em 1
             println("Fale a posição na lista: ")
             val posicao = readLine()!!.toInt()
             println("Nome da pessoa: ")
             val nome = readLine()!!
 
             convidados.put(posicao, nome)
-        } else if (opcao == 2) {
+        } else if (opcao == 2) {                           // para remover, digitando em 2
             println("Fale quem você quer tirar: ")
             var tirar = readLine()!!.toInt()
 
             convidados.remove(tirar)
 
-        } else if (opcao == 3) {
+        } else if (opcao == 3) {      // Aparece os convidados
             println(convidados)
 
-        } else if (opcao == 4) {
+        } else if (opcao == 4) {                  // se pedir para sair mandar mensagem agradecendo o teste
             println(" Obrigado por participar do Beta \uD83D\uDC96" + "\n Para ver novas atualizações confiram o link: https://github.com/cristhian-ruescas/29_10  \uD83D\uDE80")
             sair = "Não"
-        } else {
+        } else {                        // numero invalido
             print("Numero invalido")
             sair += "Não"
-            
+
         }
     }
 }
